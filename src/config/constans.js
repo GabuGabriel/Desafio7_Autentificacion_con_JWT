90d1ba502d9c465d306e017d22273704ef76e120
GabrielMunozSolis <gmunoz@wit-labs.com>
2024-11-12 04:23:32 -0300

    pushfinal

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,6 +1,6 @@
 export const URLBASE = 'http://localhost:3000'
 
 export const ENDPOINT = {
-  login: `${URLBASE}/login`,
-  users: `${URLBASE}/usuarios`
+  login: `${URLBASE}/auth/login`,
+  users: `${URLBASE}/auth/usuarios`
 }
--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,15 @@ const pool = new Pool({
   database: 'softjobs',
   password: 'postgres',
   port: 5432,
+  allowExitOnIdle: true,
+});
+
+pool.on('connect', () => {
+  console.log('Base de datos conectada con éxito');
+});
+
+pool.on('error', (err) => {
+  console.error('Error en el pool de conexiones', err);
 });
 
 module.exports = pool;
